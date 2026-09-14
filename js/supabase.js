f8567225223cf35e3bba1b3ade2d20a8398edb80
HollowKnightFan1234 <theodore.allard@gmail.com>
2026-09-13 20:53:28 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://zfdgkeqqdhbjibrbsqir.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://zfdgkeqqdhbjibrbsqir.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_g_2yqsARIYRXZ3OAUxpfig_N0jwy0Wg";
 
 window.appSupabase = window.supabase.createClient(
